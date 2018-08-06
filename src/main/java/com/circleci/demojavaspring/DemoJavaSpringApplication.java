91fe274bf30d75a6812e6682a41acb7cd4e8c022
gudsr04 <40855116+gudsr04@users.noreply.github.com>
2018-08-06 18:46:49 +0530

    Update DemoJavaSpringApplication.java

--- src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
+++ src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
@@ -1,6 +1,7 @@
 package com.circleci.demojavaspring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
